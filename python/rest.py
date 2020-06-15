5847780a6ac0a1f613a47878f8e6029d309dce11
belsanpesou <belsanpesou@yahoo.com.br>
2020-06-15 19:03:58 -0300

    Treinando mais

---
+++ python/rest.py
@@ -0,0 +1,20 @@
+import requests
+
+def consulta():
+    response = requests.get('http://127.0.0.1:5000/pessoa/')
+    print(response.status_code)
+    print(response.json())
+    for pessoa in response.json():
+        print(pessoa['id'], pessoa['nome'], pessoa['idade'])
+
+def insere():
+    nome = 'Iza'
+    idade = 400
+    pessoa = {"nome": nome, "idade": idade}
+    response = requests.post('http://127.0.0.1:5000/pessoa/', json=pessoa)
+    print(response.status_code)
+    print(response.json())
+
+
+consulta()
+insere()
