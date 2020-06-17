ef3e1a9fc74d3cb74d456ef514bff02ec1ac4b21
belsanpesou <belsanpesou@yahoo.com.br>
2020-06-16 23:56:30 -0300

    List Replication

--- haskel/fden.hs
+++ haskel/fden.hs
@@ -1,24 +1,27 @@
 import Prelude
+{-  List Replication  -}
 
-jk :: Int -> [Int] -> [Int]
-jk n arr = do
+mul :: Int -> Int -> [Int]
+mul n x = do
+    let j = x
     if n > 1 then do
-        let p = head arr         
-        jk (n-1) (p:arr)
+        j : (mul (n-1) x)
     else
-            arr
+        return j
 
-f :: Int -> [Int] -> [Int]
+
+f:: Int -> [Int] -> [Int]
 f n arr = do
-    if  (length (tail arr) > 0)  then do
-        jk n arr
-        f n (tail arr)
+    let h = head arr
+    let t = tail arr
+    let m = []
+    if (length t > 0) then do
+        m ++ (mul n h) ++ ( f n t )                
     else
-        arr
+        arr ++ (mul (n-1) h)
     
 
 main :: IO()
 main = do
-    let arr = [1,2,3,4,5]
-    let p = jk 3 arr
-    print p
+    let h = [1, 2, 3, 4, 5]
+    print ( f 3 h )
